98798f5f1b16b3d3f6ed24680a434049db20beae
merytpeters <merytpeters@gmail.com>
2024-05-15 14:39:08 +0100

    Syntax adjusted, documentations added, datetime updated to ISO format, should run as expected

--- models/base_model.py
+++ models/base_model.py
@@ -3,24 +3,20 @@
 
 
 from uuid import uuid4
-from datetime import datetime
+import datetime
 class BaseModel:
     """Class BaseModel that defines all common attributes/methods for other classes"""
 
     def __init__(self):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.id = str(uuid4())  #instantiate all public instance attribute
+        self.created_at = datetime.datetime.now().isoformat
+        self.updated_at = datetime.datetime.now().isoformat  #Converts to ISO format also
 
     def __str__(self):
-        return f"{self.__class__.name} {self.id} {self.dict}"
+        return f"[{self.__class__.name}] ({self.id}) {self.dict}"  #Converts to string
 
     def save(self):
-       self.updated_at = datetime.now()
-       str(self.created_at)
+       return self.updated_at  #updates this public instance attribute with the current time
 
-Base1 = BaseModel()
-Base2 = BaseModel()
-
-print(Base1.id)
-print(Base2.id)
+    def to_dict(self):
+        return {key: value for key, value in self.__dict__.items()}
