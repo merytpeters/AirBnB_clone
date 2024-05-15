cebf50d5488f97cd24350dffbe940348a92e1d28
merytpeters <merytpeters@gmail.com>
2024-05-15 15:34:31 +0100

    Pycodestyle errors fixed

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
 """The start of the airbnb clone"""
-
-
 from uuid import uuid4
 import datetime
-class BaseModel:
-    """Class BaseModel that defines all common attributes/methods for other classes"""
 
+
+class BaseModel:
+    """Class BaseModel that defines all common attributes/methods
+       for other classes"""
     def __init__(self):
-        self.id = str(uuid4())  #instantiate all public instance attribute
+        self.id = str(uuid4())
+        # instantiate all public instance attribute
         self.created_at = datetime.datetime.now().isoformat
-        self.updated_at = datetime.datetime.now().isoformat  #Converts to ISO format also
+        self.updated_at = datetime.datetime.now().isoformat
+        # Converts to ISO format also
 
     def __str__(self):
-        return f"[{self.__class__.name}] ({self.id}) {self.dict}"  #Converts to string
+        return f"[{self.__class__.name}] ({self.id}) {self.dict}"
+        # Converts to string
 
     def save(self):
-       return self.updated_at  #updates this public instance attribute with the current time
+        return self.updated_at
+        # updates this public instance attribute with the current time
 
     def to_dict(self):
         return {key: value for key, value in self.__dict__.items()}
