ebfcf0ce4daf25e4ed0f6ee2c8aca186d9932f39
merytpeters <merytpeters@gmail.com>
2024-05-19 20:49:43 +0100

    All fixed , file_storage, all unnecesary files deleted

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -14,37 +14,37 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def __init__(self):
-        """initialize the FileStorage class"""
-        pass
-
     def all(self):
         """Returns the dictionary __objects"""
 
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """sets in __objects the obj
         with key <obj class name>.id
         """
-        key = f"{obj.__class__.name__}.{obj.id}"
-        self.__objects[key] = obj
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file
         path: __file_path
         """
-        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(self.__file_path, 'w') as f:
+        obj_dict = {
+            key: obj.to_dict()
+            for key, obj in FileStorage.__objects.items()
+        }
+        with open(FileStorage.__file_path, 'w') as f:
             json.dump(obj_dict, f)
 
     def reload(self):
         """deserilalizes the JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r') as f:
+            with open(FileStorage.__file_path, 'r') as f:
                 obj_dict = json.load(f)
-                for key, value in obj_dict.items():
-                    class_name = value["__class__"]
-                    self.__objects[key] = self.classes[class_name](**value)
+                for key in obj_dict.values():
+                    class_name = key["__class__"]
+                    del key["__class__"]
+                    self.new(eval(class_name)(**key))
         except FileNotFoundError:
             pass
--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,13 +1,33 @@
 #!/usr/bin/python3
 """Test file module for base model"""
+from models.base_model import BaseModel
 import os
 import unittest
-from models.base_model import BaseModel
+from datetime import datetime
 
 
-class TestBaseModel(unittest.BaseModel):
+class TestBaseModel(unittest.TestCase):
     """Checks that BaseModel work as required"""
 
-
     def setUp(self):
-        self.instance = BaseModel  
+        """Checks the class exists"""
+        self.instance = BaseModel()
+
+    def test_kwargs_attributes(self):
+        """Checks for the correct time attributes and
+        that they are datetime objects and also creates
+        new instance attributes otherwise"""
+        self.assertIsInstance(self.instance.created_at, datetime)
+        self.assertIsInstance(self.instance.updated_at, datetime)
+
+    def test_new_attributes(self):
+        self.assertIsInstance(self.instance.id)
+
+    def test_id_unique(self):
+        """Test that id is unique"""
+        base1 = BaseModel()
+        self.assertIsNotEqual(self.instance.id, base1.id)
+
+
+if __name__ == '__main__':
+    unittest.main()
