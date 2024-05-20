8fee1d258a993dc633ba9d0b928554e7d7ed30cc
Pury-01 <puryokumu@gmail.com>
2024-05-20 12:50:04 +0300

    updated

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -42,9 +42,12 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 obj_dict = json.load(f)
 
-                for key in obj_dict.values():
-                    class_name = key["__class__"]
-                    del key["__class__"]
-                    self.new(eval(class_name)(**key))
+                for key, value in obj_dict.items():
+                    class_name = value["__class__"]
+                    classes = globals().get(class_name)
+                    # del key["__class__"]
+                    # self.new(eval(class_name)(**key))
+                    if classes:
+                        self.new(classes(**value))
         except FileNotFoundError:
             pass
