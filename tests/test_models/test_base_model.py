15a877c44427861cd9f7869b59a1d1aed8b1aa15
merytpeters <merytpeters@gmail.com>
2024-05-21 08:40:52 +0100

    Base Model Test File Done , one test failed

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,22 +11,43 @@ class TestBaseModel(unittest.TestCase):
 
     def setUp(self):
         """Checks the class exists"""
-        self.instance = BaseModel()
+        self.model = BaseModel()
 
     def test_kwargs_attributes(self):
         """Checks for the correct time attributes and
         that they are datetime objects and also creates
         new instance attributes otherwise"""
-        self.assertIsInstance(self.instance.created_at, datetime)
-        self.assertIsInstance(self.instance.updated_at, datetime)
+        self.assertIsInstance(self.model, BaseModel)
+        self.assertIsInstance(self.model.created_at, datetime)
+        self.assertIsInstance(self.model.updated_at, datetime)
 
     def test_new_attributes(self):
-        self.assertIsInstance(self.instance.id, str)
+        self.assertIsInstance(self.model.id, str)
 
     def test_id_unique(self):
         """Test that id is unique"""
         base1 = BaseModel()
-        self.assertNotEqual(self.instance.id, base1.id)
+        self.assertNotEqual(self.model.id, base1.id)
+
+    def test_to_dict(self):
+        model_dict = self.model.to_dict()
+        self.assertIsInstance(model_dict, dict)
+        self.assertEqual(model_dict['__class__'], 'BaseModel')
+        self.assertEqual(model_dict['created_at'], str)
+        self.assertEqual(model_dict['updated_at'], str)
+
+    def test_recreation_from_dict(self):
+        model_dict = self.model.to_dict()
+        new_model = BaseModel(**model_dict)
+        self.assertIsInstance(new_model, BaseModel)
+        self.assertEqual(new_model.id, self.model.id)
+        self.assertEqual(new_model.created_at, self.model.created_at)
+        self.assertEqual(new_model.updated_at, self.model.updated_at)
+
+    def test_save(self):
+        old_updated_at = self.model.updated_at
+        self.model.save()
+        self.assertNotEqual(self.model.updated_at, old_updated_at)
 
 
 if __name__ == '__main__':
